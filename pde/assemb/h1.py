b61b809d5efc92180e8a891ce05bf7c4caebbd8f
Bogdan Radu <catalinradu@dyn-eduroam232217.wlan.jku.at>
2023-01-24 12:25:25 +0100

    nonlinear stuff

--- pde/assemb/h1.py
+++ pde/assemb/h1.py
@@ -1,5 +1,4 @@
 
-from re import I
 from .. import quadrature
 import numpy as npy
 from scipy import sparse as sp
@@ -12,37 +11,25 @@ def get_info(MESH,space):
         
         INFO = {}
         INFO['TRIG'] = {}
-                    
+            
         if space == 'P1':
             INFO['TRIG']['space'] = 'P1'
+            INFO['TRIG']['space_dx'] = 'P0'
             INFO['TRIG']['qp_we_M'] = quadrature.dunavant(order = 2)
             INFO['TRIG']['qp_we_Mh'] = quadrature.dunavant(order = 1)
             INFO['TRIG']['qp_we_K'] = quadrature.dunavant(order = 0)
             
-        return INFO
-    
-    def get_info_quad(space):
-        INFO = {}
-        INFO['QUAD'] = {}
-        
-        if space == 'Q1':
-            INFO['QUAD']['space'] = 'Q1'
-            INFO['QUAD']['qp_we_M'] = quadrature.quadrule(order = 3)
-            INFO['QUAD']['qp_we_Mh'] = quadrature.quadrule(order = 1)
-            INFO['QUAD']['qp_we_K'] = quadrature.quadrule(order = 3)
+        if space == 'P2':
+            INFO['TRIG']['space'] = 'P2'
+            INFO['TRIG']['space_dx'] = 'P1'
+            INFO['TRIG']['qp_we_M'] = quadrature.dunavant(order = 4)
+            # INFO['TRIG']['qp_we_Mh'] = quadrature.dunavant(order = 1)
+            INFO['TRIG']['qp_we_K'] = quadrature.dunavant(order = 2)
             
         return INFO
     
-    INFO = {}   
     
-    ###########################################################################
-    if space == 'P1-Q1': 
-        INFO = INFO | get_info_trig('P1')
-        INFO = INFO | get_info_quad('Q1')
-        INFO['sizeM'] = MESH.np
-        INFO['sizeD'] = MESH.nt # +MESH.nq
-    ###########################################################################
-    
+    INFO = {}
     
     ###########################################################################
     if space == 'P1': 
@@ -66,67 +53,60 @@ def get_info(MESH,space):
 
 
 
-
-
-
-
-
-def M(MESH,BASIS,LISTS,Dict):
-    time_elapsed = time.time()
+def h1(MESH,BASIS,LISTS,Dict):
     space = Dict.get('space')
-    matrix = Dict.get('matrix')
-    
-    MAT = {}; MAT[space] = {}
-    MAT_TRIG = {}; MAT_QUAD = {}
-    
-    t = MESH.t; q = MESH.q
+    time_elapsed = time.time()
     
     INFO = get_info(MESH,space)
-
-    if t.shape[0] != 0: MAT_TRIG = h1_trig(MESH,BASIS,LISTS,INFO,Dict)
-    if q.shape[0] != 0: MAT_QUAD = h1_quad(MESH,BASIS,LISTS,INFO,Dict)
-
-    if t.shape[0] == 0: MAT[space] = MAT_QUAD
-    if q.shape[0] == 0: MAT[space] = MAT_TRIG
     
-    # MAT_BOUNDARY = h1_boundary(MESH,BASIS,LISTS,INFO)
-
-    if t.shape[0] != 0 and q.shape[0] != 0:
-        MAT_TRIG_Set = set(MAT_TRIG)
-        MAT_QUAD_Set = set(MAT_QUAD)
-        
-        for field in MAT_TRIG_Set.intersection(MAT_QUAD_Set):
-            MAT[space][field] = MAT_TRIG[field] + MAT_QUAD[field]
-            
+    MAT = h1_trig(MESH,BASIS,LISTS,INFO,Dict)
+    
     elapsed = time.time()-time_elapsed
     print('Assembling ' + space + ' took ' + str(elapsed)[0:5] + ' seconds.')
     return MAT
 
 def h1_trig(MESH,BASIS,LISTS,INFO,Dict):
+    
+    matrix = Dict.get('matrix')
+    if 'coeff' in Dict.keys():
+        coeff = Dict.get('coeff')
+    else:
+        coeff = lambda x,y : 1+x*0+y*0
+        
+    if 'coeff_const' in Dict.keys():
+        coeff_const = Dict.get('coeff_const')
+    else:
+        coeff_const = npy.ones(shape = MESH.nt)
+        
+    if 'regions' in Dict.keys():
+        regions = Dict.get('regions')
+    else:
+        regions = MESH.RegionsT
+    
+    indices = npy.argwhere(npy.in1d(MESH.RegionsT,regions))[:,0]
 
     p = MESH.p;
-    t = MESH.t; nt = MESH.nt
+    t = MESH.t[indices,:]; nt = t.shape[0]
+    
     
     spaceTrig = INFO['TRIG']['space'];
+    spaceTrig_dx = INFO['TRIG']['space_dx'];
+    sizeM = INFO['sizeM']
+    sizeD = INFO['sizeD']
     qp_M = INFO['TRIG']['qp_we_M'][0]; we_M = INFO['TRIG']['qp_we_M'][1]
     qp_K = INFO['TRIG']['qp_we_K'][0]; we_K = INFO['TRIG']['qp_we_K'][1]
 
     phi_H1 = BASIS[spaceTrig]['TRIG']['phi']; lphi_H1 = len(phi_H1)
     dphi_H1 = BASIS[spaceTrig]['TRIG']['dphi']; ldphi_H1 = len(dphi_H1)
+    phi_L2 = BASIS[spaceTrig_dx]['TRIG']['phi']; lphi_L2 = len(phi_L2)
 
-    H1_LIST_DOF = LISTS[spaceTrig]['TRIG']['LIST_DOF']
-
-    ellmatsM_H1 = npy.zeros((nt,lphi_H1*lphi_H1))
-    ellmatsM_H1_LUMPED = npy.zeros((nt,lphi_H1*lphi_H1))
-    
-    ellmatsKx_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
-    ellmatsKy_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
+    H1_LIST_DOF = LISTS[spaceTrig]['TRIG']['LIST_DOF'][indices,:]
+    H1_DX_LIST_DOF = LISTS[spaceTrig_dx]['TRIG']['LIST_DOF'][indices,:]
 
     phiix_H1 = npy.zeros((nt,lphi_H1))
     phiiy_H1 = npy.zeros((nt,lphi_H1))
-    phii_H1 = npy.zeros((nt,lphi_H1)) 
-
-    MAT = {}
+    phii_H1 = npy.zeros((nt,lphi_H1))
+    phii_L2 = npy.zeros((nt,lphi_L2))
 
     #####################################################################################
     # Mappings
@@ -137,173 +117,121 @@ def h1_trig(MESH,BASIS,LISTS,INFO,Dict):
     A10 = p[t1,1]-p[t0,1]; A11 = p[t2,1]-p[t0,1]
     detA = A00*A11-A01*A10
     
-    #####################################################################################
-    # Mass matrix
-    #####################################################################################
-
-    for i in range(len(we_M)):
-        for j in range(lphi_H1):
-            phii_H1[:,j] = phi_H1[j](qp_M[0,i],qp_M[1,i])
-
-        ellmatsM_H1 = ellmatsM_H1 + 1/2*we_M[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detA)[:,None]
-
     im_H1,jm_H1 = create_indices(H1_LIST_DOF,H1_LIST_DOF)
-    ellmatsM_H1[abs(ellmatsM_H1)<1e-14] = 0
-    MAT['M'] = sparse(im_H1,jm_H1,ellmatsM_H1,INFO['sizeM'],INFO['sizeM'])
-    MAT['M'].eliminate_zeros()
-
-    #####################################################################################
-    # Stiffness matrix
-    #####################################################################################
-
-    for i in range(len(we_K)):
-        for j in range(lphi_H1):
-            dphii_H1 = dphi_H1[j](qp_K[0,i],qp_K[1,i])
-            phiix_H1[:,j] = 1/detA*( A11*dphii_H1[0] -A10*dphii_H1[1])
-            phiiy_H1[:,j] = 1/detA*(-A01*dphii_H1[0] +A00*dphii_H1[1])
-
-        ellmatsKx_H1 = ellmatsKx_H1 + 1/2*we_K[i]*assem_ellmats(phiix_H1,phiix_H1)*npy.abs(detA)[:,None]
-        ellmatsKy_H1 = ellmatsKy_H1 + 1/2*we_K[i]*assem_ellmats(phiiy_H1,phiiy_H1)*npy.abs(detA)[:,None]
-
-    ellmatsKx_H1[abs(ellmatsKx_H1)<1e-14] = 0
-    ellmatsKy_H1[abs(ellmatsKy_H1)<1e-14] = 0
-    
-    MAT['Kx'] = sparse(im_H1,jm_H1,ellmatsKx_H1,INFO['sizeM'],INFO['sizeM']); MAT['Kx'].eliminate_zeros()
-    MAT['Ky'] = sparse(im_H1,jm_H1,ellmatsKy_H1,INFO['sizeM'],INFO['sizeM']); MAT['Ky'].eliminate_zeros()
-    
-    MAT['M'].eliminate_zeros()
-        
-    #####################################################################################
-    # Lumped mass matrix
-    #####################################################################################
-
-    # if 'qp_Mh' in locals():
-    if 'qp_we_Mh' in INFO['TRIG']:
-        qp_Mh = INFO['TRIG']['qp_we_Mh'][0]; we_Mh = INFO['TRIG']['qp_we_Mh'][1]
-        for i in range(len(we_Mh)):
-            for j in range(lphi_H1):
-                phii_H1[:,j] = phi_H1[j](qp_Mh[0,i],qp_Mh[1,i])
-
-            ellmatsM_H1_LUMPED = ellmatsM_H1_LUMPED + 1/2*we_M[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detA)[:,None]
-        
-        ellmatsM_H1_LUMPED[abs(ellmatsM_H1_LUMPED)<1e-14] = 0
-        MAT['Mh'] = sparse(im_H1,jm_H1,ellmatsM_H1_LUMPED,INFO['sizeM'],INFO['sizeM'])
-        MAT['Mh'].eliminate_zeros()
-    return MAT
-
-
-
-
-
-def h1_quad(MESH,BASIS,LISTS,INFO):
-    p = MESH.p; q = MESH.q; nq = MESH.nq
-    
-    spaceQuad = INFO['QUAD']['space'];
-    qp_M = INFO['QUAD']['qp_we_M'][0]; we_M = INFO['QUAD']['qp_we_M'][1]
-    qp_K = INFO['QUAD']['qp_we_K'][0]; we_K = INFO['QUAD']['qp_we_K'][1]
-
-    phi_H1 = BASIS[spaceQuad]['QUAD']['phi']; lphi_H1 = len(phi_H1)
-    dphi_H1 = BASIS[spaceQuad]['QUAD']['dphi']; ldphi_H1 = len(dphi_H1)
-
-    H1_LIST_DOF = LISTS[spaceQuad]['QUAD']['LIST_DOF'] + INFO['list_shift']
-
-    ellmatsM_H1 = npy.zeros((nq,lphi_H1*lphi_H1))
-    ellmatsM_H1_LUMPED = npy.zeros((nq,lphi_H1*lphi_H1))
-    
-    ellmatsKx_H1 = npy.zeros((nq,ldphi_H1*ldphi_H1))
-    ellmatsKy_H1 = npy.zeros((nq,ldphi_H1*ldphi_H1))
-
-    phiix_H1 = npy.zeros((nq,lphi_H1))
-    phiiy_H1 = npy.zeros((nq,lphi_H1))
-    phii_H1 = npy.zeros((nq,lphi_H1))
-    
-    MAT = {}
-
-    #####################################################################################
-    # Mappings
-    #####################################################################################
-    
-    q0 = q[:,0]; q1 = q[:,1]; q2 = q[:,2]; q3 = q[:,3]
-    
-    B00 = p[q1,0]-p[q0,0]; B01 = p[q3,0]-p[q0,0]
-    B10 = p[q1,1]-p[q0,1]; B11 = p[q3,1]-p[q0,1]
-    detB = abs(B00*B11-B01*B10)
-    
-    C00 = p[q2,0]-p[q3,0]; C01 = p[q3,0]-p[q0,0]
-    C10 = p[q2,1]-p[q3,1]; C11 = p[q3,1]-p[q0,1]
-    detC = abs(C00*C11-C01*C10)
-    
-    D00 = p[q1,0]-p[q0,0]; D01 = p[q2,0]-p[q1,0]
-    D10 = p[q1,1]-p[q0,1]; D11 = p[q2,1]-p[q1,1]
-    detD = abs(D00*D11-D01*D10)
-    
-    detQ = lambda x,y: detB + (detD-detB)*x + (detC-detB)*y
-    
-    R0 = p[q2,0]-p[q3,0]-p[q1,0]+p[q0,0]
-    R1 = p[q2,1]-p[q3,1]-p[q1,1]+p[q0,1]
-    
-    Q00 = lambda x,y : B00+R0*y; Q01 = lambda x,y : B01+R1*x
-    Q10 = lambda x,y : B10+R0*y; Q11 = lambda x,y : B11+R1*x # not 100% sure on this one ...    
     
     #####################################################################################
     # Mass matrix
     #####################################################################################
     
-    MAT = {}
+    if matrix == 'M':
+        ellmatsM_H1 = npy.zeros((nt,lphi_H1*lphi_H1))
+        
+        for i in range(len(we_M)):
+            qpT_i_1 = A00*qp_M[0,i]+A01*qp_M[1,i]+p[t0,0]
+            qpT_i_2 = A10*qp_M[0,i]+A11*qp_M[1,i]+p[t0,1]
+            coeff_qpT_i = coeff(qpT_i_1,qpT_i_2)
+            for j in range(lphi_H1):
+                phii_H1[:,j] = phi_H1[j](qp_M[0,i],qp_M[1,i])
     
-    for i in range(len(we_M)):
-        detQi = detQ(qp_M[0,i],qp_M[1,i])        
-        for j in range(lphi_H1):
-            phii_H1[:,j] = phi_H1[j](qp_M[0,i],qp_M[1,i])
-
-        ellmatsM_H1 = ellmatsM_H1 + we_M[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detQi)[:,None]
-
-    im_H1,jm_H1 = create_indices(H1_LIST_DOF,H1_LIST_DOF)
-    ellmatsM_H1[abs(ellmatsM_H1)<1e-14] = 0
-    MAT['M'] = sparse(im_H1,jm_H1,ellmatsM_H1,INFO['sizeM'],INFO['sizeM'])
-    MAT['M'].eliminate_zeros()
+            ellmatsM_H1 = ellmatsM_H1 + 1/2*we_M[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+    
+        ellmatsM_H1[abs(ellmatsM_H1)<1e-14] = 0
+        M = sparse(im_H1,jm_H1,ellmatsM_H1,sizeM,sizeM)
+        M.eliminate_zeros()
+        return M
 
     #####################################################################################
     # Stiffness matrix
     #####################################################################################
 
-    for i in range(len(we_K)):
-        detQi = detQ(qp_K[0,i],qp_K[1,i])
-        Q00i = Q00(qp_K[0,i],qp_K[1,i]); Q10i = Q10(qp_K[0,i],qp_K[1,i])
-        Q01i = Q01(qp_K[0,i],qp_K[1,i]); Q11i = Q11(qp_K[0,i],qp_K[1,i])
-
-        for j in range(lphi_H1):
-            dphii_H1 = dphi_H1[j](qp_K[0,i],qp_K[1,i])
-            phiix_H1[:,j] = 1/detQi*( Q11i*dphii_H1[0] -Q10i*dphii_H1[1])
-            phiiy_H1[:,j] = 1/detQi*(-Q01i*dphii_H1[0] +Q00i*dphii_H1[1])
-
-        ellmatsKx_H1 = ellmatsKx_H1 + we_K[i]*assem_ellmats(phiix_H1,phiix_H1)*npy.abs(detQi)[:,None]
-        ellmatsKy_H1 = ellmatsKy_H1 + we_K[i]*assem_ellmats(phiiy_H1,phiiy_H1)*npy.abs(detQi)[:,None]
+    if matrix == 'K':
+        ellmatsKxx_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
+        ellmatsKyy_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
+        ellmatsKxy_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
+        ellmatsKyx_H1 = npy.zeros((nt,ldphi_H1*ldphi_H1))
         
-    ellmatsKx_H1[abs(ellmatsKx_H1)<1e-14] = 0
-    ellmatsKy_H1[abs(ellmatsKy_H1)<1e-14] = 0
-   
-    MAT['Kx'] = sparse(im_H1,jm_H1,ellmatsKx_H1,INFO['sizeM'],INFO['sizeM']); MAT['Kx'].eliminate_zeros()
-    MAT['Ky'] = sparse(im_H1,jm_H1,ellmatsKy_H1,INFO['sizeM'],INFO['sizeM']); MAT['Ky'].eliminate_zeros()
+        for i in range(len(we_K)):
+            qpT_i_1 = A00*qp_K[0,i]+A01*qp_K[1,i]+p[t0,0]
+            qpT_i_2 = A10*qp_K[0,i]+A11*qp_K[1,i]+p[t0,1]
+            coeff_qpT_i = coeff(qpT_i_1,qpT_i_2)
+            
+            for j in range(lphi_H1):
+                dphii_H1 = dphi_H1[j](qp_K[0,i],qp_K[1,i])
+                phiix_H1[:,j] = 1/detA*( A11*dphii_H1[0] -A10*dphii_H1[1])
+                phiiy_H1[:,j] = 1/detA*(-A01*dphii_H1[0] +A00*dphii_H1[1])
+    
+            ellmatsKxx_H1 = ellmatsKxx_H1 + 1/2*we_K[i]*assem_ellmats(phiix_H1,phiix_H1)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            ellmatsKyy_H1 = ellmatsKyy_H1 + 1/2*we_K[i]*assem_ellmats(phiiy_H1,phiiy_H1)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            ellmatsKxy_H1 = ellmatsKxy_H1 + 1/2*we_K[i]*assem_ellmats(phiix_H1,phiiy_H1)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            ellmatsKyx_H1 = ellmatsKyx_H1 + 1/2*we_K[i]*assem_ellmats(phiiy_H1,phiix_H1)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            
+        ellmatsKxx_H1[abs(ellmatsKxx_H1)<1e-14] = 0
+        ellmatsKyy_H1[abs(ellmatsKyy_H1)<1e-14] = 0
+        ellmatsKxy_H1[abs(ellmatsKxy_H1)<1e-14] = 0
+        ellmatsKyx_H1[abs(ellmatsKyx_H1)<1e-14] = 0
+        
+        Kxx = sparse(im_H1,jm_H1,ellmatsKxx_H1,sizeM,sizeM); Kxx.eliminate_zeros()
+        Kyy = sparse(im_H1,jm_H1,ellmatsKyy_H1,sizeM,sizeM); Kyy.eliminate_zeros()
+        Kxy = sparse(im_H1,jm_H1,ellmatsKxy_H1,sizeM,sizeM); Kxy.eliminate_zeros()
+        Kyx = sparse(im_H1,jm_H1,ellmatsKyx_H1,sizeM,sizeM); Kyx.eliminate_zeros()
+        
+        return Kxx, Kyy, Kxy, Kyx
 
+    #####################################################################################
+    # grad trafo matrix
+    #####################################################################################
+
+    if matrix == 'C':
+        ellmatsCx_H1 = npy.zeros((nt,lphi_H1*lphi_L2))
+        ellmatsCy_H1 = npy.zeros((nt,lphi_H1*lphi_L2))
+        
+        for i in range(len(we_M)):
+            qpT_i_1 = A00*qp_M[0,i]+A01*qp_M[1,i]+p[t0,0]
+            qpT_i_2 = A10*qp_M[0,i]+A11*qp_M[1,i]+p[t0,1]
+            coeff_qpT_i = coeff(qpT_i_1,qpT_i_2)
+            
+            for j in range(lphi_H1):
+                dphii_H1 = dphi_H1[j](qp_M[0,i],qp_M[1,i])
+                phiix_H1[:,j] = 1/detA*( A11*dphii_H1[0] -A10*dphii_H1[1])
+                phiiy_H1[:,j] = 1/detA*(-A01*dphii_H1[0] +A00*dphii_H1[1])
+            for j in range(lphi_L2):
+                phii_L2[:,j] = phi_L2[j](qp_M[0,i],qp_M[1,i])
+    
+            ellmatsCx_H1 = ellmatsCx_H1 + 1/2*we_M[i]*assem_ellmats(phiix_H1,phii_L2)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            ellmatsCy_H1 = ellmatsCy_H1 + 1/2*we_M[i]*assem_ellmats(phiiy_H1,phii_L2)*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            
+        ellmatsCx_H1[abs(ellmatsCx_H1)<1e-14] = 0
+        ellmatsCy_H1[abs(ellmatsCy_H1)<1e-14] = 0
+        
+        ic_H1,jc_H1 = create_indices(H1_LIST_DOF,H1_DX_LIST_DOF)
+        Cx = sparse(ic_H1,jc_H1,ellmatsCx_H1,sizeD,sizeM); Cx.eliminate_zeros()
+        Cy = sparse(ic_H1,jc_H1,ellmatsCy_H1,sizeD,sizeM); Cy.eliminate_zeros()
+        return Cx,Cy
+        
     #####################################################################################
     # Lumped mass matrix
     #####################################################################################
 
-    # if 'qp_Mh' in locals():
-    if 'qp_we_Mh' in INFO['QUAD']:
-        qp_Mh = INFO['QUAD']['qp_we_Mh'][0]; we_Mh = INFO['QUAD']['qp_we_Mh'][1]
-        for i in range(len(we_Mh)):
-            detQi = detQ(qp_Mh[0,i],qp_Mh[1,i])        
-            for j in range(lphi_H1):
-                phii_H1[:,j] = phi_H1[j](qp_Mh[0,i],qp_Mh[1,i])
+    if matrix == 'Mh':
+        ellmatsM_H1_LUMPED = npy.zeros((nt,lphi_H1*lphi_H1))
+        
+        if 'qp_we_Mh' in INFO['TRIG']:
+            qp_Mh = INFO['TRIG']['qp_we_Mh'][0]; we_Mh = INFO['TRIG']['qp_we_Mh'][1]
+            for i in range(len(we_Mh)):
+                qpT_i_1 = A00*qp_K[0,i]+A01*qp_K[1,i]+p[t0,0]
+                qpT_i_2 = A10*qp_K[0,i]+A11*qp_K[1,i]+p[t0,1]
+                coeff_qpT_i = coeff(qpT_i_1,qpT_i_2)
+                for j in range(lphi_H1):
+                    phii_H1[:,j] = phi_H1[j](qp_Mh[0,i],qp_Mh[1,i])
+    
+                ellmatsM_H1_LUMPED = ellmatsM_H1_LUMPED + 1/2*we_M[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detA)[:,None]*coeff_qpT_i[:,None]*coeff_const[indices,None]
+            
+            ellmatsM_H1_LUMPED[abs(ellmatsM_H1_LUMPED)<1e-14] = 0
+            Mh = sparse(im_H1,jm_H1,ellmatsM_H1_LUMPED,sizeM,sizeM)
+            Mh.eliminate_zeros()
+            return Mh
+        
 
-            ellmatsM_H1_LUMPED = ellmatsM_H1_LUMPED + we_Mh[i]*(assem_ellmats(phii_H1,phii_H1))*npy.abs(detQi)[:,None]
-             
-        ellmatsM_H1_LUMPED[abs(ellmatsM_H1_LUMPED)<1e-14] = 0
-        MAT['Mh'] = sparse(im_H1,jm_H1,ellmatsM_H1_LUMPED,INFO['sizeM'],INFO['sizeM'])
-        MAT['Mh'].eliminate_zeros()
-    return MAT
 
 
 
@@ -326,9 +254,9 @@ def h1b(MESH,BASIS,LISTS,Dict):
     
     INFO = get_info(MESH,space)
     qp_B = INFO['qp_we_B'][0]; we_B = INFO['qp_we_B'][1]
-    phi_H1_B = BASIS[space]['phi']; lphi_H1_B = len(phi_H1_B)
+    phi_H1_B = BASIS[space]['B']['phi']; lphi_H1_B = len(phi_H1_B)
     
-    H1_BOUNDARY_LIST_DOF = LISTS[space]['LIST_DOF'][indices,:]
+    H1_BOUNDARY_LIST_DOF = LISTS[space]['B']['LIST_DOF'][indices,:]
     ellmatsB_H1 = npy.zeros((ne,lphi_H1_B*lphi_H1_B))
     phii_H1_B = npy.zeros((ne,lphi_H1_B))
     
