fa635fd2bc897a8f9ad0ef32970996cd43646278
Bogdan Radu <radubogdan2@gmail.com>
2023-01-31 15:17:46 +0100

    noice

---
+++ sanity_checks/eigenmodes.py
@@ -0,0 +1,84 @@
+import sys
+sys.path.insert(0,'..') # adds parent directory
+sys.path.insert(0,'../CEM') # adds parent directory
+
+import numpy as np
+import gmsh
+import pde
+import scipy.sparse as sps
+import scipy.sparse.linalg
+import time
+import geometries
+
+import plotly.io as pio
+pio.renderers.default = 'browser'
+from matplotlib.pyplot import spy
+
+
+np.set_printoptions(threshold = np.inf)
+np.set_printoptions(linewidth = np.inf)
+np.set_printoptions(precision = 8)
+
+gmsh.initialize()
+gmsh.model.add("Capacitor plates")
+geometries.unitSquare()
+gmsh.option.setNumber("Mesh.Algorithm", 2)
+gmsh.option.setNumber("Mesh.MeshSizeMax", 0.2)
+gmsh.option.setNumber("Mesh.MeshSizeMin", 0.2)
+
+f = lambda x,y : (np.pi**2+np.pi**2)*np.sin(np.pi*x)*np.sin(np.pi*y)
+g = lambda x,y : 0*x
+u = lambda x,y : np.sin(np.pi*x)*np.sin(np.pi*y)
+
+p,e,t,q = pde.petq_generate()
+MESH = pde.mesh(p,e,t,q)
+MESH.makeFemLists()
+    
+tm = time.time()
+
+# Mass & Stifness    
+Kx,Ky = pde.h1.assemble(MESH, space = 'P1', matrix = 'K', order = 0)
+D0 = pde.int.assemble(MESH, order = 0)
+
+MB = pde.h1.assemble(MESH, space = 'P1', matrix = 'M', order = 2)
+D2 = pde.int.assemble(MESH, order = 2)
+
+Kxx = Kx@D0@Kx.T; Kyy = Ky@D0@Ky.T; M = MB@D2@MB.T
+
+ff = pde.int.evaluate(MESH, coeff = f, order = 2)
+M_f = MB@D2@ ff.diagonal()
+
+# Boundary stuff
+Mb = pde.h1.assembleB(MESH, space = 'P1', matrix = 'M', shape = Kxx.shape, order = 2)
+Db0 = pde.int.assembleB(MESH, order = 2)
+B_full = Mb@Db0@Mb.T
+
+D_g = pde.int.evaluateB(MESH, order = 2, coeff = g)
+B_g = Mb@Db0@ D_g.diagonal()
+
+gamma = 10**10
+
+A = Kxx + Kyy + gamma*B_full
+b = gamma*B_g + M_f
+
+u_ex = u(MESH.p[:,0],MESH.p[:,1])
+elapsed = time.time()-tm; print('Assembling stuff took ' + str(elapsed)[0:5] + ' seconds.')
+
+u = MB@D2@ pde.int.evaluate(MESH, coeff = f, order = 2).diagonal()
+
+sigma = 1
+# x = sps.linalg.eigs(Kxx+Kyy-sigma*M+gamma*B_full,M = M, sigma = sigma)
+x = sps.linalg.eigsh(Kxx + Kyy + gamma*B_full, M = M, sigma = 2, which='SM', maxiter=1e6)
+phi = np.real(x[1][:,0])
+
+print(x[0])
+
+# tm = time.time()
+# uh = sps.linalg.spsolve(A,b)
+# elapsed = time.time()-tm
+# print('Solving took ' + str(elapsed)[0:5] + ' seconds.')
+
+
+fig = MESH.pdesurf_hybrid(dict(trig = 'P1', controls = 1), phi)
+fig.show()
+
--- unused/numba_tries/1.py
+++ unused/numba_tries/1.py
@@ -1,13 +1,16 @@
-# from numba import jit as njit
 import numba as nb
 import numpy as np
 import time
 
-# @nb.jit(parallel=True,cache=True)
+@nb.jit(parallel=True,cache=True)
 def ident_np(x):
     return np.cos(x) ** 2 + np.sin(x) ** 2
-
 # @nb.jit(parallel=True,cache=True)
+
+def ident_np2(x):
+    return np.cos(x) ** 2 + np.sin(x) ** 2
+
+@nb.jit(cache=True)
 def ident_loops(x):
     r = np.empty_like(x)
     n = len(x)
@@ -17,12 +20,16 @@ def ident_loops(x):
 
 
 
-x = np.random.rand(100_000_000)
+x = np.random.rand(1_000_000_000)
 
 tm = time.time()
 ident_np(x)
 print(time.time()-tm)
 
 tm = time.time()
-ident_loops(x)
+ident_np2(x)
 print(time.time()-tm)
+
+# tm = time.time()
+# ident_loops(x)
+# print(time.time()-tm)
