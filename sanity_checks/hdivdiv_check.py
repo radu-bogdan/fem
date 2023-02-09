9eb1d1de76b0b881077fc52dadcbc9aaa91c2fa3
Bogdan Radu <radubogdan2@gmail.com>
2023-02-09 23:25:17 +0100

    new

--- sanity_checks/hdivdiv_check.py
+++ sanity_checks/hdivdiv_check.py
@@ -14,18 +14,21 @@ from sksparse.cholmod import cholesky
 import plotly.io as pio
 pio.renderers.default = 'browser'
 from matplotlib.pyplot import spy
+import matplotlib.pyplot as plt
 
 
 np.set_printoptions(threshold = np.inf)
 np.set_printoptions(linewidth = np.inf)
-np.set_printoptions(precision = 8)
+np.set_printoptions(precision = 2)
 
 gmsh.initialize()
 gmsh.model.add("Capacitor plates")
 geometries.unitSquare()
 gmsh.option.setNumber("Mesh.Algorithm", 2)
-gmsh.option.setNumber("Mesh.MeshSizeMax", 0.01)
-gmsh.option.setNumber("Mesh.MeshSizeMin", 0.01)
+# gmsh.option.setNumber("Mesh.MeshSizeMax", 1)
+# gmsh.option.setNumber("Mesh.MeshSizeMin", 1)
+gmsh.option.setNumber("Mesh.MeshSizeMax", 1)
+gmsh.option.setNumber("Mesh.MeshSizeMin", 1)
 
 p,e,t,q = pde.petq_generate()
 MESH = pde.mesh(p,e,t,q)
@@ -43,55 +46,160 @@ A = cholesky(K)
 
 N = A.L()@A.L().T
 
-# spy(N)
 
-# # block_ends = np.zeros()
+# def fast_inverse(A):
+#     identity = np.identity(A.shape[2], dtype=A.dtype)
+#     Ainv = np.zeros_like(A)
 
-# arr = np.empty(shape = (0,N.shape[0]))
-# for i in range(N.shape[0]):
-    
-#     N_diag = N.diagonal(k=-i) # Nebendiagonale anfangen
-    
-#     block_ends = np.argwhere(abs(N_diag)<1e-10)[:,0]
-    
-#     if np.linalg.norm(N_diag)<1e-10:
-#         break
-    
-#     values = np.r_[N.diagonal(k=-i),np.zeros(i)]
-#     arr = np.r_[arr, values[None]]
+#     for i in range(A.shape[0]):
+#         Ainv[i] = np.linalg.solve(A[i], identity)
+#     return Ainv
 
+# @profile
 
-# from scipy.linalg import solveh_banded
-# D = scipy.sparse.eye(N.shape[0])
-# invN = solveh_banded(arr, D.toarray() , lower=True)
-
-# print(arr.shape)
-# spy(invN)
-# # N = A.L()@A.L().T
-
-
-
-
+# def invert_block(N):
+        
+#####################################################################################
+# Find indices where the blocks begin/end
+#####################################################################################
 tm = time.time()
+
 N_diag = N.diagonal(k=-1) # Nebendiagonale anfangen
-block_ends = np.argwhere(abs(N_diag)<1e-6)[:,0]
+block_ends = np.r_[np.argwhere(abs(N_diag)==0)[:,0],N.shape[0]-1]
 
 for i in range(N.shape[0]):
-    N_diag = np.r_[N.diagonal(k=-(i+2)),np.zeros(i)]
-    arg = np.argwhere(abs(N_diag[block_ends])>1e-10)[:,0]
-    # print(arg)
-    # print(block_ends)
-    block_ends = np.delete(block_ends,arg).copy()
-    # print(block_ends)
+    N_diag = np.r_[N.diagonal(k=-(i+2)),np.zeros(i+2)]
     
-    if np.linalg.norm(N_diag)<1e-10:
-        break
+    for j in range(i+1):
+        arg = np.argwhere(abs(N_diag[block_ends-j])>0)[:,0]
+        block_ends = np.delete(block_ends,arg).copy()
+        
+    if np.linalg.norm(N_diag)==0: break
+
+block_ends = np.r_[-1,block_ends]
+block_lengths = np.r_[block_ends[1:]-block_ends[0:-1]]
+
+elapsed = time.time()-tm; print('Preparing lists {:4.8f} seconds.'.format(elapsed))
+#####################################################################################
 
 
-elapsed = time.time()-tm; print('Solving took  {:4.8f} seconds.'.format(elapsed))
+#####################################################################################
+# Inversion of the blocks:
+#####################################################################################
+iN = sps.lil_matrix(N.shape)
+
+tm = time.time()
+for i in range(len(block_ends)-1):
+    
+    C = N[block_ends[i]+1:block_ends[i+1]+1,
+          block_ends[i]+1:block_ends[i+1]+1].toarray()
+    
+    iC = np.linalg.inv(C)
+    
+    iN[block_ends[i]+1:block_ends[i+1]+1,
+       block_ends[i]+1:block_ends[i+1]+1] = iC
+    
+elapsed = time.time()-tm; print('Inverting took {:4.8f} seconds.'.format(elapsed))
+#####################################################################################
 
 
-for i in range(block_ends):
+#####################################################################################
+# Inversion of the blocks, 2nd try.
+#####################################################################################
+
+dataN = N.data
+indicesN = N.indices
+indptrN = N.indptr    
+lengths = N.indptr[1:]-N.indptr[0:-1]
+
+print(indicesN)
+print(indptrN)
+
+
+# ind = np.empty(0,dtype=int)
+# for i,val in enumerate(leng):
+#     ind = np.append(ind,np.tile(i,val))
+
+# print(ind)
+# print(ind.shape)
+print(indicesN.shape)
+print('dasdasd')
+
+pp = 0
+
+block_ends = block_ends + 1
+
+for i,ii in enumerate(block_ends):
+    
+    print(i,ii)
     
     
+    CC = np.zeros(shape=(block_lengths[i],block_lengths[i]))
     
+    for k in range(ii):
+        # print(k)
+        print(indicesN[indptrN[k]:indptrN[k+1]])
+        CC[k-ii,indicesN[indptrN[k]:indptrN[k+1]]] = dataN[indptrN[k]:indptrN[k+1]]
+    
+    
+    # for k in 
+    
+    # pos = np.argwhere((N.indices>block_ends[i]) & (N.indices<=block_ends[i+1]))[:,0]
+    # indices = indicesN[pos]
+    
+    # C = dataN[np.argwhere((N.indices>block_ends[i]) & (N.indices<=block_ends[i+1]))]
+    
+    # CC = np.zeros(shape=(block_lengths[i],block_lengths[i]))
+    
+    # print(lengths)
+        
+    # for i in range()
+    #     CC[] = dataN[k*(j+1)]
+    
+    
+    print(CC)
+    # print(indices)
+#####################################################################################
+
+# return iN
+
+
+
+
+
+
+# iN = invert_block(N)
+
+# tm = time.time()
+# iN2 = sps.linalg.inv(N)
+# elapsed = time.time()-tm; print('Inverting in the classic way took {:4.8f} seconds.'.format(elapsed))
+    
+# print(sps.linalg.norm(iN2-iN))
+
+print(N.shape)
+
+m = 1
+plt.close('all')
+plt.figure(); spy(N,markersize = m)
+plt.figure(); spy(iN,markersize = m)
+
+import numpy as np
+
+mat = np.zeros((5, 5))
+indexes = np.array([[0, 2], [1, 3], [1, 4], [0, 2], [1, 4]])
+values = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+
+for row_i, column_indices in enumerate(indexes):
+    print(row_i)
+    print(column_indices)
+    mat[row_i, column_indices] = values[row_i]
+
+
+
+# plt.figure(); spy(N,markersize = m)
+# plt.figure(); spy(iN-iN2,precision=1e-12,markersize = m)
+
+
+
+# print(iN2[41:41+12,41:41+12].toarray(),'\n\n')
+# print(N[41:41+12,41:41+12].toarray())
