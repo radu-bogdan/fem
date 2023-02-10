412d82a023e26f54a824b4ce04433836efc677f5
Bogdan Radu <radubogdan2@gmail.com>
2023-02-10 16:46:45 +0100

    new

--- sanity_checks/hdivdiv_check.py
+++ sanity_checks/hdivdiv_check.py
@@ -28,8 +28,8 @@ geometries.unitSquare()
 gmsh.option.setNumber("Mesh.Algorithm", 2)
 gmsh.option.setNumber("Mesh.MeshSizeMax", 1)
 gmsh.option.setNumber("Mesh.MeshSizeMin", 1)
-gmsh.option.setNumber("Mesh.MeshSizeMax", 0.002)
-gmsh.option.setNumber("Mesh.MeshSizeMin", 0.002)
+gmsh.option.setNumber("Mesh.MeshSizeMax", 0.01)
+gmsh.option.setNumber("Mesh.MeshSizeMin", 0.01)
 
 p,e,t,q = pde.petq_generate()
 MESH = pde.mesh(p,e,t,q)
@@ -60,85 +60,118 @@ N = A.L()@A.L().T
 
 # @profile
 
-def invert_block(N):
+# def invert_block(N):
         
-    N = N.tocsc()
-    
-    #####################################################################################
-    # Find indices where the blocks begin/end
-    #####################################################################################
-    tm = time.time()
-    
-    N_diag = N.diagonal(k=-1) # Nebendiagonale anfangen
-    block_ends = np.r_[np.argwhere(abs(N_diag)==0)[:,0],N.shape[0]-1]
-    
-    for i in range(N.shape[0]):
-        N_diag = np.r_[N.diagonal(k=-(i+2)),np.zeros(i+2)]
-        
-        for j in range(i+1):
-            arg = np.argwhere(abs(N_diag[block_ends-j])>0)[:,0]
-            block_ends = np.delete(block_ends,arg).copy()
-            
-        if np.linalg.norm(N_diag)==0: break
-    
-    block_ends = np.r_[-1,block_ends]
-    block_lengths = np.r_[block_ends[1:]-block_ends[0:-1]]
-    
-    elapsed = time.time()-tm; print('Preparing lists {:4.8f} seconds.'.format(elapsed))
-    #####################################################################################
-    
-    
-    #####################################################################################
-    # Inversion of the blocks:
-    #####################################################################################
-    iN = sps.lil_matrix(N.shape)
-    
-    tm = time.time()
-    for i in range(len(block_ends)-1):
-        
-        C = N[block_ends[i]+1:block_ends[i+1]+1,
-              block_ends[i]+1:block_ends[i+1]+1].toarray()
-        
-        iC = np.linalg.inv(C)
-        
-        iN[block_ends[i]+1:block_ends[i+1]+1,
-           block_ends[i]+1:block_ends[i+1]+1] = iC
-        
-    elapsed = time.time()-tm; print('Inverting1 took {:4.8f} seconds.'.format(elapsed))
-    #####################################################################################
-    
-    iN2 = 0
-    
-    #####################################################################################
-    # Inversion of the blocks, 2nd try.
-    #####################################################################################
-    # tm = time.time(); A = ()
-    # for i in range(len(block_ends)-1):
-    #     C = N[block_ends[i]+1:block_ends[i+1]+1,
-    #           block_ends[i]+1:block_ends[i+1]+1].toarray()
-        
-    #     iC = np.linalg.inv(C)
-        
-    #     A += (iC,)
-    
-    # from scipy.sparse import block_diag as bd_sp
-    # iN2 = bd_sp(A)
-    # elapsed = time.time()-tm; print('Inverting2 took {:4.8f} seconds.'.format(elapsed))
-    #####################################################################################
+N = N.tocsc()
 
-    return iN,iN2
+#####################################################################################
+# Find indices where the blocks begin/end
+#####################################################################################
+tm = time.time()
+
+N_diag = N.diagonal(k=-1) # Nebendiagonale anfangen
+block_ends = np.r_[np.argwhere(abs(N_diag)==0)[:,0],N.shape[0]-1]
+
+for i in range(N.shape[0]):
+    N_diag = np.r_[N.diagonal(k=-(i+2)),np.zeros(i+2)]
+    
+    for j in range(i+1):
+        arg = np.argwhere(abs(N_diag[block_ends-j])>0)[:,0]
+        block_ends = np.delete(block_ends,arg).copy()
+        
+    if np.linalg.norm(N_diag)==0: break
+
+block_ends = np.r_[0,block_ends+1]
+
+elapsed = time.time()-tm; print('Preparing lists {:4.8f} seconds.'.format(elapsed))
+#####################################################################################
+
+
+#####################################################################################
+# Inversion of the blocks:
+#####################################################################################
+iN = sps.lil_matrix(N.shape)
+
+tm = time.time()
+for i,ii in enumerate(block_ends[:-1]):
+    
+    C = N[block_ends[i]:block_ends[i+1],
+          block_ends[i]:block_ends[i+1]].toarray()
+    
+    iC = np.linalg.inv(C)
+    
+    iN[block_ends[i]:block_ends[i+1],
+       block_ends[i]:block_ends[i+1]] = iC
+    
+elapsed = time.time()-tm; print('Inverting1 took {:4.8f} seconds.'.format(elapsed))
+#####################################################################################    
 
 
 
+#####################################################################################
+# Inversion of the blocks, 2nd try.
+#####################################################################################
+
+dataN = N.data
+indicesN = N.indices
+indptrN = N.indptr
+block_lengths = np.r_[block_ends[1:]-block_ends[0:-1]]
+
+@nb.njit()
+def whatever(dataN,indicesN,indptrN,block_ends,block_lengths):
+    C = np.zeros(sum(block_lengths**2))
+    for i,ii in enumerate(block_ends[:-1]):
+        CC = np.zeros(shape=(block_lengths[i],block_lengths[i]),dtype=np.float64)
+        
+        for k in range(block_lengths[i]):
+            in_k = np.arange(start = indptrN[block_ends[i]+k], stop = indptrN[block_ends[i]+k+1], step = 1, dtype = np.int64)
+            for j,jj in enumerate(in_k):
+                CC[k,indicesN[jj]-block_ends[i]] = dataN[jj]
+    
+        iCC = np.linalg.inv(CC)
+        
+        C[sum((block_lengths**2)[0:i]):sum((block_lengths**2)[0:i+1])] = iCC.flatten()
+        # nur noch die indices and im done :) 
+        
+    return C
 
 
-iN,iN2 = invert_block(N)
+
+# C = np.zeros(sum(block_lengths**2))
+# for i,ii in enumerate(block_ends[:-1]):
+#     CC = np.zeros(shape=(block_lengths[i],block_lengths[i]),dtype=np.float64)
+    
+#     for k in range(block_lengths[i]):
+#         in_k = np.arange(start = indptrN[block_ends[i]+k], stop = indptrN[block_ends[i]+k+1], step = 1, dtype = np.int64)
+#         for j,jj in enumerate(in_k):
+#             CC[k,indicesN[jj]-block_ends[i]] = dataN[jj]
+
+
+#     iCC = np.linalg.inv(CC)
+    
+#     C[sum((block_lengths**2)[0:i]):sum((block_lengths**2)[0:i+1])] = iCC.flatten()
+    
+    # pause
+
+tm = time.time()
+C = whatever(dataN,indicesN,indptrN,block_ends,block_lengths)        
+elapsed = time.time()-tm; print('Njit took {:4.8f} seconds.'.format(elapsed))
+#####################################################################################
+
+# iN2 = 0
+# return iN,iN2
+
+
+
+
+
+# iN,iN2 = invert_block(N)
 
 # tm = time.time()
 # iN2 = sps.linalg.inv(N)
 # elapsed = time.time()-tm; print('Inverting in the classic way took {:4.8f} seconds.'.format(elapsed))
     
-print('kekw', sps.linalg.norm(iN2-iN))
+# print('kekw', sps.linalg.norm(iN2-iN))
 
 print(N.shape)
 
@@ -157,42 +190,3 @@ plt.figure(); spy(iN,markersize = m)
 # print(iN2[41:41+12,41:41+12].toarray(),'\n\n')
 # print(N[41:41+12,41:41+12].toarray())
 
-
-
-
-
-
-
-
-def numbaspeed(dataN,indicesN,indptrN,block_ends,block_lengths):
-    block_ends = block_ends + 1
-    A = ()
-    
-    for i,ii in enumerate(block_ends[:-1]):
-        
-        CC = np.zeros(shape=(block_lengths[i],block_lengths[i]),dtype=np.float64)
-        
-        for k in range(block_lengths[i]):
-            in_k = np.empty(shape=(0,),dtype=np.int64)
-            
-            for el in range(indptrN[block_ends[i]+k+1]-indptrN[block_ends[i]+k]):
-                in_k = np.append(in_k,indptrN[block_ends[i]+k]+el)
-            
-            # in_k = np.r_[indptrN[block_ends[i]+k]:indptrN[block_ends[i]+k+1]]
-            # print(in_k)
-            
-            # print(k,indicesN[in_k]-block_ends[i],dataN[in_k])
-            # print(CC[k,:])
-            # print(CC[:,indicesN[in_k]-block_ends[i]])
-            
-            # CCk = CC[k,:]
-            # CCk[indicesN[in_k]-block_ends[i]] = dataN[in_k]
-            # CC[k,:] = CCk
-            
-            
-            CC[k,indicesN[in_k]-block_ends[i]] = dataN[in_k]
-            
-            
-        iCC = np.linalg.inv(CC)
-        A += (iCC,)
-    return A
