2deb19a3c70b1517d4182698262e75692a1e4953
Bogdan Radu <radubogdan2@gmail.com>
2023-06-21 20:30:45 +0200

    Update nonlinLaws.py

--- PROJECTS/SeminarEM/nonlinLaws.py
+++ PROJECTS/SeminarEM/nonlinLaws.py
@@ -51,6 +51,54 @@ def fyy_nonlinear(x,y):
 
 # IDEE: vectorize???
 
+def gx_gy_nonlinear_vek(x,y):
+    le = x.size
+    Hn = np.array([x,y])
+    Hn0 = Hn[0,:]; Hn1 = Hn[1,:]
+    
+    Bn = np.zeros((2,le))+1
+    Bn0 = Bn[0,:]; Bn1 = Bn[1,:];
+    Bn = np.sqrt(Bn0**2+Bn1**2)*Hn/np.sqrt(Hn0**2+Hn1**2)
+    
+    for it in range(10000):
+        fxx = fxx_nonlinear(Bn0,Bn1); fxy = fxy_nonlinear(Bn0,Bn1)
+        fyx = fyx_nonlinear(Bn0,Bn1); fyy = fyy_nonlinear(Bn0,Bn1)
+        fx  = fx_nonlinear (Bn0,Bn1); fy  = fy_nonlinear (Bn0,Bn1)
+        
+        inv_detF = 1/(fxx*fyy-fxy*fyx)
+        inv_F_xx =-inv_detF*fyy; inv_F_xy = inv_detF*fxy
+        inv_F_yx = inv_detF*fyx; inv_F_yy =-inv_detF*fxx
+        
+        # w = inv_Fxx@(Hnk-Fx)
+        
+        w0 = inv_F_xx*(Hn0-fx)+inv_F_xy*(Hn1-fy)
+        w1 = inv_F_yx*(Hn0-fx)+inv_F_yy*(Hn1-fy)
+        
+        alpha = np.zeros((1,le))+1
+        
+        for r in range(1000):
+            fxu = fx_nonlinear(Bn0-alpha*w0,Bn1-alpha*w1)
+            fyu = fy_nonlinear(Bn0-alpha*w0,Bn1-alpha*w1)
+            
+            # print(((fxu-Hn0)**2+(fyu-Hn1)**2)-((fx-Hn0)**2+(fy-Hn1)**2))
+            # to_check = ((fxu-Hn0)**2+(fyu-Hn1)**2)-((fx-Hn0)**2+(fy-Hn1)**2)
+            if (((fxu-Hn0)**2+(fyu-Hn1)**2)-((fx-Hn0)**2+(fy-Hn1)**2)).any()>0: break
+            else: alpha = alpha*(1/2)
+            
+        Bn0u = Bn0 - alpha*w0
+        Bn1u = Bn1 - alpha*w1
+        
+        if (np.sqrt((Bn1u-Bn1)**2+(Bn0u-Bn0)**2)).all()<1e-5:
+            break
+        
+        Bn0 = Bn0u.copy()
+        Bn1 = Bn1u.copy()
+        
+        if it==(10000-1):
+            print("did not converge!")
+            
+    return Bn0,Bn1
+
 ##########################################################################################
 @nb.njit((float64[:],float64[:]),fastmath=False, parallel=True, cache=True)
 def gx_gy_nonlinear(x,y):
@@ -123,23 +171,24 @@ import time
 # a = np.random.rand(1_000_000)
 # b = np.random.rand(1_000_000)
 
-a = np.random.randint(100_000, size = 10_000_000).astype(float)
-b = np.random.randint(100_000, size = 10_000_000).astype(float)
+a = np.random.randint(100_000, size = 1_000).astype(float)
+b = np.random.randint(100_000, size = 1_000).astype(float)
 
 tm = time.monotonic(); g,gx,gy,gxx,gxy,gyx,gyy = g_nonlinear_all(a,b); print(time.monotonic()-tm)
 
 for i in range(10000):
-    a = np.random.randint(100_000, size = 10_000_000).astype(float)
-    b = np.random.randint(100_000, size = 10_000_000).astype(float)
-    tm = time.monotonic(); gx,gy = gx_gy_nonlinear(a,b); print(time.monotonic()-tm)
+    a = np.random.randint(100_000, size = 1_000).astype(float)
+    b = np.random.randint(100_000, size = 1_000).astype(float)
+    # tm = time.monotonic(); gx,gy = gx_gy_nonlinear(a,b); print(time.monotonic()-tm)
+    tm = time.monotonic(); gx,gy = gx_gy_nonlinear_vek(a,b); print(time.monotonic()-tm)
 
 ##########################################################################################
 
     err = np.linalg.norm((fx_nonlinear(gx,gy)-a)**2+\
                          (fy_nonlinear(gx,gy)-b)**2)
     print("inverse g' of f'. err: ",err)
-    if err>1:
-        break
+    # if err>1:
+        # break
 ##########################################################################################
 
 
