fixed scaling

PROJECTS/TEAM/nonlin_TEAM13.py
@@ -4,8 +4,12 @@ from scipy import interpolate
 import numba as nb
 
 nu0 = 10**7/(4*np.pi)
-# k1 = 49.4; k2 = 1.3; k3 = 520.6
-k1 = 49.4; k2 = 1.46; k3 = 520.6
+
+# k1 = 49.4; k2 = 1.3; k3 = 120.6 # 21
+
+k1 = 49.4; k2 = 1.46; k3 = 520.6 # 21
+# k1 = 2.6; k2 = 2.72; k3 = 154.4 # 17
+# k1 = 3.8; k2 = 2.17; k3 = 396.2 # 21
 
 @nb.njit()
 def gen_nu(x):
@@ -13,13 +17,13 @@ def gen_nu(x):
     pos = 1/k2*np.log(1/k1*(nu0-k3))
     return nu_nl(x)*(x<pos)+nu0*(x>pos)
 
-xx = np.linspace(0,8,500)
-yy = np.exp(np.linspace(0,np.log(5*1e8),500))-1
+xx = np.linspace(0,8,5000)
+yy = np.exp(np.linspace(0,np.log(5*1e8),5000))-1
 
 nu = interpolate.CubicSpline(xx, gen_nu(xx), bc_type = 'natural')
 spl = interpolate.make_smoothing_spline(xx, nu.derivative()(xx))
-dx_nu = interpolate.BSpline(xx, spl(xx)*(spl(xx)>0),k=1)
-# dx_nu = nu.derivative(1)
+# dx_nu = interpolate.BSpline(xx, spl(xx)*(spl(xx)>0),k=1)
+dx_nu = nu.derivative(1)
 
 gen_gs = interpolate.CubicSpline(xx*nu(xx**2), xx, bc_type = 'natural')
 mu = interpolate.CubicSpline(yy[1:]**2, gen_gs(yy[1:])/yy[1:], bc_type = 'natural')

PROJECTS/TEAM/solve_t13_mag_pot_nl.py
@@ -3,7 +3,7 @@ sys.path.insert(0,'../../') # adds parent directory
 from scipy import sparse as sp
 
 from solve_t13_strom import *
-from nonlin_TEAM13_new import *
+from nonlin_TEAM13 import *
 
 MESH = pde.mesh3.netgen(geoOCCmesh)
 
@@ -38,8 +38,9 @@ curlphix_Hcurl, curlphiy_Hcurl, curlphiz_Hcurl = pde.hcurl.assemble3(MESH, space
 aJ = jx_hdiv @ D @ phix_Hcurl.T +\
      jy_hdiv @ D @ phiy_Hcurl.T +\
      jz_hdiv @ D @ phiz_Hcurl.T
-     
-aJ = 1e7*aJ
+
+nu0 = 10**7/(4*np.pi)
+aJ = nu0*aJ
 
 def gss(A):
     curl_Ax = curlphix_Hcurl.T@A; curl_Ay = curlphiy_Hcurl.T@A; curl_Az = curlphiz_Hcurl.T@A
@@ -150,11 +151,16 @@ Bz = curlphiz_Hcurl_P0.T @ A
 # Storing to vtk
 ##############################################################################
 
-import vtklib
+# import vtklib
+# grid = vtklib.createVTK(MESH)
+# vtklib.add_H1_Scalar(grid, potential_H1, 'potential_H1')
+# vtklib.add_L2_Vector(grid,jx_L2,jy_L2,jz_L2,'j_L2')
+# vtklib.add_L2_Vector(grid,Bx,By,Bz,'B')
+# vtklib.writeVTK(grid, 'das2.vtu')
 
-grid = vtklib.createVTK(MESH)
-vtklib.add_H1_Scalar(grid, potential_H1, 'potential_H1')
-vtklib.add_L2_Vector(grid,jx_L2,jy_L2,jz_L2,'j_L2')
-vtklib.add_L2_Vector(grid,Bx,By,Bz,'B')
 
-vtklib.writeVTK(grid, 'das2.vtu')
+grid = pde.tools.vtklib.createVTK(MESH)
+pde.tools.add_H1_Scalar(grid, potential_H1, 'potential_H1')
+pde.tools.add_L2_Vector(grid,jx_L2,jy_L2,jz_L2,'j_L2')
+pde.tools.add_L2_Vector(grid,Bx,By,Bz,'B')
+pde.tools.vtklib.writeVTK(grid, 'das2.vtu')

PROJECTS/TEAM/solve_t13_scal_pot_nl.py
@@ -3,7 +3,7 @@ sys.path.insert(0,'../../') # adds parent directory
 from scipy import sparse as sps
 
 from solve_t13_strom import *
-from nonlin_TEAM13_new import *
+from nonlin_TEAM13 import *
 
 MESH = pde.mesh3.netgen(geoOCCmesh)
 
@@ -23,10 +23,10 @@ Bx = curlphix_Hcurl.T @ A
 By = curlphiy_Hcurl.T @ A
 Bz = curlphiz_Hcurl.T @ A
 
-#richtig skalieren evtl!
-Hjx = Bx*10**7
-Hjy = By*10**7
-Hjz = Bz*10**7
+nu0 = 10**7/(4*np.pi)
+Hjx = Bx*nu0
+Hjy = By*nu0
+Hjz = Bz*nu0
 
 ##############################################################################
 # Assembly
@@ -113,11 +113,11 @@ for i in range(maxIter):
     u_old_i = u
     u = u + alpha*w
     
-    print ("NEWTON: Iteration: %2d " %(i+1)+"||obj: %2e" %J(u)+"|| ||grad||: %2e" %np.linalg.norm(RS @ gs(u),np.inf)+"||alpha: %2e" % (alpha)+"|| J(u) : %2e" %J(u))
+    print ("NEWTON: Iteration: %2d " %(i+1)+"||obj: %2e" %J(u)+"|| ||grad||: %2e" %np.linalg.norm(RS @ gs(u),np.inf)+"||alpha: %2e" % (alpha))
     
     # if ( np.linalg.norm(RS @ gs(u),np.inf) < eps_newton):
     #     break
-    if (np.abs(J(u)-J(u_old_i)) < 1e-1):
+    if (np.abs(J(u)-J(u_old_i)) < 1e-5):
         break
     
 elapsed = time.monotonic()-tm2
@@ -132,9 +132,9 @@ By_P0 = curlphiy_Hcurl_P0.T @ A
 Bz_P0 = curlphiz_Hcurl_P0.T @ A
 
 #richtig skalieren evtl!
-Hjx_P0 = Bx_P0*10**7
-Hjy_P0 = By_P0*10**7
-Hjz_P0 = Bz_P0*10**7
+Hjx_P0 = Bx_P0*nu0
+Hjy_P0 = By_P0*nu0
+Hjz_P0 = Bz_P0*nu0
 
 
 Hx = Hjx_P0 + ux