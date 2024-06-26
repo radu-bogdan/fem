88f3c64d35d199cdecd305b4c7ad1e2b36a2d3b4
Bogdan Radu <radubogdan2@gmail.com>
2024-06-26 17:50:26 +0200

    new

--- PROJECTS/TEAM/ngsimpl/nonlinear/do.py
+++ PROJECTS/TEAM/ngsimpl/nonlinear/do.py
@@ -1,7 +1,6 @@
 import ngsolve as ngs
 from ttictoc import tic, toc
 from ngsolve.krylovspace import CGSolver
-
 from ngcotree import *
 
 
@@ -27,17 +26,16 @@ def solve(HCurl,A,mesh,deg,J,fun_w,fun_dw,fun_ddw):
     B = ngs.curl(A)
     normB = ngs.sqrt(B*B + regb)
 
+    ir = ngs.IntegrationRule(ngs.fem.ET.TET, order = 2*deg)
 
     cf_energy = mesh.MaterialCF({linear: nu0/2*B*B, nonlinear: fun_w(normB)}, default = nu0/2*B*B).Compile()
 
     def fun_W():
-        with ngs.TaskManager(): res = ngs.Integrate(cf_energy - J*A, mesh)
-        # with ngs.TaskManager(): res = ngs.Integrate(cf_energy - J*A, mesh, order = 2*deg)
+        # with ngs.TaskManager(): res = ngs.Integrate(cf_energy - J*A, mesh)
+        with ngs.TaskManager(): res = ngs.Integrate(cf_energy - J*A, mesh, order = 2*deg)
         # with ngs.TaskManager(): res = ngs.Integrate(cf_energy - ngs.curl(Hs)*A, mesh)
         # print("res:" + str(ngs.Integrate(cf_energy, mesh)))
         return res
-
-    ir = ngs.IntegrationRule(ngs.fem.ET.TET, order = 2*deg)
 
 
     cf_rhs = mesh.MaterialCF({linear: nu0, nonlinear: fun_dw(normB)/normB}, default = nu0).Compile()
@@ -52,7 +50,6 @@ def solve(HCurl,A,mesh,deg,J,fun_w,fun_dw,fun_ddw):
         with ngs.TaskManager(): rhs.Assemble()
         return rhs
 
-
     Id = ngs.CF((1,0,0,
                  0,1,0,
                  0,0,1), dims=(3,3))
@@ -60,7 +57,6 @@ def solve(HCurl,A,mesh,deg,J,fun_w,fun_dw,fun_ddw):
     BBt = ngs.CF((B[0]*B[0], B[0]*B[1], B[0]*B[2],
                   B[1]*B[0], B[1]*B[1], B[1]*B[2],
                   B[2]*B[0], B[2]*B[1], B[2]*B[2]), dims = (3,3))
-
 
     fun1 = fun_dw(normB)/normB
     fun2 = (fun_ddw(normB) - fun_dw(normB)/normB)/(normB*normB)
@@ -153,5 +149,131 @@ def solve(HCurl,A,mesh,deg,J,fun_w,fun_dw,fun_ddw):
     # HDiv = ngs.HDiv(mesh, order = deg)
     # B = ngs.GridFunction(HDiv)
     # with ngs.TaskManager(): B.Set(ngs.curl(A))
+
+    return A,it
+
+
+
+def solve_2d(H1,A,mesh,deg,J,fun_w,fun_dw,fun_ddw):
+    linear = "coil_plus|coil_minus"
+    nonlinear = "stator"
+
+    mu0 = 1.256636e-6
+    nu0 = 1/mu0
+
+    u,v = H1.TnT()
+
+    rot = ngs.CF((0,1,-1,0), dims=(2,2))
+    def curl2d(a):
+        return rot*ngs.grad(a)
+
+    
+    maxit = 100000
+    tol2 = 1e-8
+    regb = 1e-8
+
+    B = curl2d(A)
+    normB = ngs.sqrt(B*B + regb)
+
+    ir = ngs.IntegrationRule(ngs.fem.ET.TRIG, order = 2*deg)
+
+    cf_energy = mesh.MaterialCF({linear: nu0/2*B*B, nonlinear: fun_w(normB)}, default = nu0/2*B*B).Compile()
+
+    def fun_W():
+        # with ngs.TaskManager(): res = ngs.Integrate(cf_energy - curl2d(A)*Hs, mesh, order = 2*deg)
+        with ngs.TaskManager(): res = ngs.Integrate(cf_energy - A*J, mesh, order = 2*deg)
+        return res
+
+
+    cf_rhs = mesh.MaterialCF({linear: nu0, nonlinear: fun_dw(normB)/normB}, default = nu0).Compile()
+
+    rhs = ngs.LinearForm(H1)
+    rhs += ngs.SymbolicLFI(cf_rhs*B*curl2d(v) - J*v, intrule = ir)
+
+    def fun_dW(): #implicitly depending on A!
+        with ngs.TaskManager(): rhs.Assemble()
+        return rhs
+
+    Id = ngs.CF((1,0,
+                 0,1), dims = (2,2))
+
+    BBt = ngs.CF((B[0]*B[0], B[0]*B[1],
+                  B[1]*B[0], B[1]*B[1]), dims = (2,2))
+
+    fun1 = fun_dw(normB)/normB
+    fun2 = (fun_ddw(normB) - fun_dw(normB)/normB)/(normB*normB)
+
+    cf_iter = mesh.MaterialCF({linear: nu0*Id, nonlinear: fun1*Id + fun2*BBt}, default = nu0*Id).Compile()
+
+    K_iter = ngs.BilinearForm(H1)
+    K_iter += ngs.SymbolicBFI(cf_iter*curl2d(u)*curl2d(v), intrule = ir)
+    C_iter = ngs.Preconditioner(K_iter, type = "local")
+
+    def fun_ddW():
+        with ngs.TaskManager(): K_iter.Assemble()
+        return K_iter
+    
+    print("Using 2D mesh with ne=", mesh.ne, "elements and nv=", mesh.nv, "points and " ,H1.ndof, "DOFs.\n ")
+
+    with ngs.TaskManager(): A.Set(ngs.CF((0)))
+
+    du = ngs.GridFunction(H1)
+    uo = ngs.GridFunction(H1)
+    wo = 1e12
+
+    for it in range(1,maxit+1):
+        tic()
+        # with ngs.TaskManager():
+        #     K_iter.Assemble()
+        #     rhs.Assemble()
+        #     res = ngs.Integrate(cf_energy - Hs*ngs.curl(A), mesh)
+        
+        w  = fun_W()
+        dw = fun_dW()
+        da = fun_ddW()
+        tm1 = toc()
+        
+        tic()
+
+        with ngs.TaskManager():
+            du.vec.data = da.mat.Inverse(H1.FreeDofs(), inverse = "sparsecholesky") * dw.vec
+            iterativeSolver = CGSolver(K_iter.mat, freedofs = H1.FreeDofs(), atol = 1e-2,  maxiter = maxit, printrates = False)
+            # iterativeSolver = CGSolver(K_iter.mat, pre = C_iter.mat, tol  = 1e-8,  maxiter = maxit)
+            # du.vec.data = iterativeSolver * dw.vec
+        
+        if len(iterativeSolver.residuals) == maxit: print("... Failure!")
+        # print(f"Number of iterations = {iterativeSolver.iterations}/{maxit} | Residual = {iterativeSolver.residuals[-1]}")
+        tm2 = toc()
+
+        nrm = ngs.InnerProduct(du.vec,dw.vec)
+        
+        if it == 1:
+            nrm0 = nrm
+        
+        # wn = 1e12
+        if abs(wo-w)/abs(w) < tol2:
+        # if abs(wn-w) < tol2:
+        # if nrm/nrm0 < tol2:
+            # print("converged to desired tolerance")
+            break
+        # elif abs(wo-w)< tol2*1e-2:
+        #     # print(abs(wo-w),abs(w),alpha,nrm,nrm0)
+        #     print("stopped early due to stagnation | energy= %.10f" %w)
+        #     # print("Iter: %2d | assem : %.2fs | CG took %.2fs with %4d iterations | alpha : %.2f | energy = %.10f | relres = %.2e |"  %(it,tm1,tm2,iterativeSolver.iterations,alpha,w,nrm/nrm0))
+        #     break
+        else:
+            # linesearch
+            uo.vec.data = A.vec.data
+            wo = w
+            alpha = 1
+            for init in range(1,21):
+                A.vec.data -= alpha*du.vec.data
+                wn = fun_W()
+                if wn < w - alpha*0.01*nrm:
+                    # print("Iter: %2d | assem : %.2fs | CG took %.2fs | alpha : %.2f | energy = %.10f | relres = %.2e |"  %(it,tm1,tm2,alpha,w,nrm/nrm0))
+                    break
+                else:
+                    alpha = alpha/2
+                    A.vec.data = uo.vec.data
 
     return A,it
